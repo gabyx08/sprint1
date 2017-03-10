a00550347a26b1cef8ce361c820615e017ad7722
Gaby <gaby.ranarz@gmail.com>
2017-03-10 15:34:23 -0600

    ultimoproyecto1

--- js/app.js
+++ js/app.js
@@ -2,21 +2,20 @@ var estudiantes = [];
 
 function obtenerListaEstudiantes() {
     // TO DO: Retornar la lista de estudiantes
-    var lista="";
-    for (var i = 0; i < estudiantes.length; i++) {
-      lista+= estudiantes[i].nombre + "\n";
-    }
-    return lista;
+  return estudiantes;
 }
 
 function agregarEstudiante() {
     // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
-    var estudiante= {
-      nombre: prompt("Ingresa el nombre del estudiante"),
-      tecnico: prompt("Ingresa el porcentaje tecnico del estudiante"),
-      hse:prompt("Ingresa el porcentaje en habilididades socio-emocionales del esudiante")
+    var nombre = prompt("Ingresa el nombre del estudiante").toUpperCase();
+    var tecnico = prompt("Ingresa los puntos tecnicos del estudiante");
+    var hse = prompt("Ingresa los puntos en habilididades socio-emocionales del estudiante");
+    var estudiante = {
+      nombre: nombre,
+      tecnico: tecnico,
+      hse:hse
     };
     estudiantes.push(estudiante);
     console.log(estudiantes);
@@ -45,11 +44,11 @@ function mostrar(estudiante) {
 function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
-
-    for (var i = 0; i < estudiantes.length; i++) {
-      return mostrar(estudiantes);
-    }
-
+    var lista= "";
+    estudiantes.forEach(function(estudiante){
+      lista += mostrar(estudiante);
+    });
+    return lista;
 
 }
 
@@ -57,26 +56,26 @@ function buscar(nombre, estudiantes) {
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
-    var encontrado= estudiantes.indexOf(nombre.toLowerCase());
-    return mostrar(encontrado);
+    nombre = nombre.toUpperCase();
+    var encontrado = estudiantes.filter(function(estudiante){
+      return nombre === estudiante.nombre;
+    });
+    return encontrado;
+
 }
 
 function topTecnico(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-    var temporal;
-    for (var i = 0; i < estudiantes.length; i++) {
-      for (var j = 0; j < estudiantes.length; j++) {
-        if(estudiantes[j].tecnico<estudiantes[j+1].tecnico){
-          temporal= estudiantes[j].tecnico;
-          estudiantes[j].tecnico=estudiantes[j+1].tecnico;
-          estudiantes[j+1].tecnico= temporal;
-        }
-      }
-      return mostrar(estudiantes)
-    }
+    var tecnicosOrdenados= estudiantes.sort(function(a,b){
+      return b.tecnico - a.tecnico;
+    });
+    return tecnicosOrdenados;
 }
 
 function topHSE(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-
+    var hseOrdenados= estudiantes.sort(function(a,b){
+      return b.hse - a.hse;
+    });
+    return hseOrdenados;
 }
